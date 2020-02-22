8441474a02774411653e0f4e73335bf7250bddcf
liamm <49158467+peterrrre@users.noreply.github.com>
2020-02-22 16:27:06 +0800

    Create z-test.js

---
+++ meetworld/src/z-test.js
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-base/legacy',
+    'plugin:flowtype/recommended',
+    'prettier',
+    'prettier/flowtype'
+  ],
+  plugins: [
+    'flowtype',
+    'prettier'
+  ],
+  // add your custom rules here
+  'rules': {
+    "class-methods-use-this": 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+
+    'prettier/prettier': ['error', {
+      'useTabs': false,
+      'printWidth': 80,
+      'tabWidth': 2,
+      'singleQuote': true,
+      'trailingComma': 'all',
+      'bracketSpacing': false
+    }]
+  }
+}
